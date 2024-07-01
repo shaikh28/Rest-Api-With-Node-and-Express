6a3318dac85cbb78cd4cb216ac0b0b804363f441
Amaan's Git <shaikhhunt7262@gamil.com>
2024-07-01 16:28:01 +0530

    added find by id endpoint

--- index.js
+++ index.js
@@ -4,8 +4,21 @@ const users = require('./MOCK_DATA.json')
 const app = express() 
 const port = 3000;
 
-app.get('/',(req,res)=>{
+app.get('/api/users',(req,res)=>{
    return res.json(users)
 })
 
+app.get('/users',(req,res)=>{
+    const html = `
+    <ul>
+    ${users.map(user=>`<li>${user.first_name}</li>`).join('')}
+    </ul>
+    `
+    return res.send(html)
+})
+app.get('/api/users/:id',(req,res)=>{
+    const id = Number(req.params.id)
+    const user = users.find((user)=>user.id === id)
+    return res.json(user)
+})
 app.listen(port,()=>console.log(`Sercer listening on http://localhost:${port}`))
