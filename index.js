e79bca97ad35dd316b332944c4c6bcac041f500d
Amaan's Git <shaikhhunt7262@gamil.com>
2024-07-02 16:21:58 +0530

    Added middleware

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 const express = require('express')
 const users = require('./MOCK_DATA.json')
-
-const app = express() 
+const fs = require('fs')
+const app = express()
 const port = 3000;
 
-app.get('/api/users',(req,res)=>{
-   return res.json(users)
+app.use(express.urlencoded({extended:false}))
+app.get('/api/users', (req, res) => {
+    return res.json(users)
 })
 
-app.get('/users',(req,res)=>{
+app.get('/users', (req, res) => {
     const html = `
     <ul>
-    ${users.map(user=>`<li>${user.first_name}</li>`).join('')}
+    ${users.map(user => `<li>${user.first_name}</li>`).join('')}
     </ul>
     `
     return res.send(html)
 })
-app.get('/api/users/:id',(req,res)=>{
-    const id = Number(req.params.id)
-    const user = users.find((user)=>user.id === id)
-    return res.json(user)
+app.route('/api/users/:id')
+    .get((req, res) => {
+        const id = Number(req.params.id)
+        const user = users.find((user) => user.id === id)
+        return res.json(user)
+    }).patch((req, res) => {
+        res.json({status:'pending'})
+
+    }).delete((req, res) => {
+        res.json({status:'pending'})
+    })
+app.post('/api/users',(req, res) => {
+    const body = req.body
+    users.push({...body,id:users.length + 1})
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+
+        res.json({status:'Success',id:users.lengt})
+    })
+
 })
-app.listen(port,()=>console.log(`Sercer listening on http://localhost:${port}`))
+app.listen(port, () => console.log(`Server listening on http://localhost:${port}`))
